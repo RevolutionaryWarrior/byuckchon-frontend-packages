41a283e0bc57480c39bf4cdc6eb9ce7dbf238cf1
Hyuk <poiu03290@naver.com>
2025-09-20 23:54:01 +0900

    feat: ActiveButton 작업

---
+++ src/packages/basic-ui/src/ActiveButton/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+type Variant = "primary" | "secondary" | "disabled";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+  variant?: Variant;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const baseButtonVariants = {
+  primary: {
+    bg: "bg-[#ED6D01] border-[#ED6D01]",
+    text: "text-white",
+  },
+  secondary: {
+    bg: "bg-white border-[#ED6D01]",
+    text: "text-[#ED6D01]",
+  },
+  disabled: {
+    bg: "bg-[#F5F5F5] border-transparent",
+    text: "text-[#8F9098]",
+  },
+};
+
+export default function ActiveButton({
+  children,
+  disabled = false,
+  variant = "primary",
+  className = "",
+  ...props
+}: Props) {
+  const variantKey = disabled ? "disabled" : variant;
+  const mergedStyle = {
+    ...baseButtonVariants[variantKey],
+  };
+
+  return (
+    <button
+      {...props}
+      disabled={disabled}
+      className={`${className} ${mergedStyle.bg} w-full cursor-pointer`}
+    >
+      <p className={`${mergedStyle.text} text-center`}>{children}</p>
+    </button>
+  );
+}
