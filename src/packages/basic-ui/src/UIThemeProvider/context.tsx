632a5f9dacb36cbdda62fef547474dfd4d5fd873
Hyuk <poiu03290@naver.com>
2025-10-16 16:46:33 +0900

    fix: 충돌 수정

--- src/packages/basic-ui/src/UIThemeProvider/context.tsx
+++ src/packages/basic-ui/src/UIThemeProvider/context.tsx
@@ -2,11 +2,17 @@ import { createContext } from "react";
 
 import type { ButtonTheme } from "../ActiveButton/theme";
 import type { AlertModalTheme } from "../AlertModal/theme";
+import type { CheckboxTheme } from "../Checkbox/theme";
 import type { ChoiceModalTheme } from "../ChoiceModal/theme";
 import type { ConfirmModalTheme } from "../ConfirmModal/theme";
+import type { PaginationTheme } from "../Pagination/theme";
+import type { ToggleTheme } from "../Toggle/theme";
 
 export type Theme = {
   button?: ButtonTheme;
+  toggle?: ToggleTheme;
+  pagination?: PaginationTheme;
+  checkbox?: CheckboxTheme;
   confirmModal?: ConfirmModalTheme;
   choiceModal?: ChoiceModalTheme;
   alertModal?: AlertModalTheme;
