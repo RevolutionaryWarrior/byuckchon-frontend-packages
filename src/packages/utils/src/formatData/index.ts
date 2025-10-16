1a6f1f17c4e634b3d274d6498d08e4e29813ec61
mlnls <tmdals140@naver.com>
2025-10-16 23:26:10 +0900

    feat: formatData 기본 구조 제작

---
+++ src/packages/utils/src/formatData/index.ts
@@ -0,0 +1,17 @@
+export const FormatData = () => {
+  const formatPhoneNumber = (value: number) => {};
+
+  const formatTelNumber = (value: number) => {};
+
+  const formatBizNumber = (value: numbber) => {};
+
+  const formatDate = (value: Date) => {};
+
+  const maskName = (value: string) => {};
+
+  const maskPhoneNumber = (value: number) => {};
+
+  const commanizeData = (value: string | number) => {};
+
+  const decommanizeData = (value: string | number) => {};
+};
