c10b32f7e90792432d68c366fcc176a4282df18f
mlnls <tmdals140@naver.com>
2025-11-10 13:32:12 +0900

    feat: Default 상태 구현

--- src/packages/basic-ui/src/Calendar/index.tsx
+++ src/packages/basic-ui/src/Calendar/index.tsx
@@ -12,15 +12,26 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 const CalendarUi = () => {
   const [value, onChange] = useState<Value>(new Date());
 
+  const checkWeekend = ({ date, view }: { date: Date; view: string }) => {
+    if (view !== "month") return;
+    const day = date.getDay();
+
+    if (day === 0) return "cal-sun";
+    if (day === 6) return "cal-sat";
+  };
+
   return (
     <div>
       <Calendar
         className="custom-calendar"
         onChange={onChange}
         value={value}
+        calendarType="gregory"
+        locale="ko-KR"
         formatDay={(_, date) => moment(date).format("D")}
         formatYear={(_, date) => moment(date).format("YYYY")}
         formatMonthYear={(_, date) => moment(date).format("YYYY. MM")}
+        tileClassName={checkWeekend}
       />
     </div>
   );
