73ddbd063fc939135cd4a6583a91b8e7fa5bf8ac
mlnls <tmdals140@naver.com>
2025-11-12 11:51:16 +0900

    feat: index 추가

--- src/packages/hooks/src/index.ts
+++ src/packages/hooks/src/index.ts
@@ -1,2 +1,4 @@
 export { default as useDetectClose } from "./useDetectClose";
 export { default as usePagination } from "./usePagination";
+export { default as useIntersectionObserver } from "./useIntersectionObserver";
+export { default as useInfiniteScroll } from "./useInfiniteScroll";
