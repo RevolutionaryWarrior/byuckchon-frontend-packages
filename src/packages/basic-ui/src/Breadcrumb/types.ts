c4a3fe4db998db18f82dd264d4054692f66fb655
SuweonPark <76687666+kksltv123@users.noreply.github.com>
2025-12-02 22:49:23 +0900

    modify: 파일 분리

---
+++ src/packages/basic-ui/src/Breadcrumb/types.ts
@@ -0,0 +1,22 @@
+export type BreadcrumbItem = {
+  label: React.ReactNode | string;
+  icon?: React.ReactNode;
+  active?: boolean;
+  onClick?: () => void;
+};
+
+export type LocalNavBreadcrumbProps = {
+  items: BreadcrumbItem[];
+  labelStyle?: string;
+  isSeparator?: boolean;
+  separatorColor?: string;
+  separatorActiveColor?: string;
+};
+
+export type ProgressBreadcrumbProps = {
+  items: BreadcrumbItem[];
+  labelStyle?: string;
+  isSeparator?: boolean;
+  separatorColor?: string;
+  separatorActiveColor?: string;
+};
