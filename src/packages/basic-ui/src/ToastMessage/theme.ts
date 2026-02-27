6fbafad9e573ecf41df8d1c248b2cb1a338bca1b
SuweonPark <76687666+kksltv123@users.noreply.github.com>
2026-02-27 12:29:00 +0900

    feat: 토스트 UI border theme 추가

--- src/packages/basic-ui/src/ToastMessage/theme.ts
+++ src/packages/basic-ui/src/ToastMessage/theme.ts
@@ -2,17 +2,21 @@ export type ToastMessageTheme = {
   default?: {
     bg?: string;
     text?: string;
+    border?: string;
   };
   error?: {
     bg?: string;
     text?: string;
+    border?: string;
   };
   success?: {
     bg?: string;
     text?: string;
+    border?: string;
   };
   warning?: {
     bg?: string;
     text?: string;
+    border?: string;
   };
 };
